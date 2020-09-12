ad025540478216ea3ec583ced2e016c6eb4b841b
Bare Necessities <oluwatosinJAjao@outlook.com>
2020-09-12 09:58:15 -0700

    Finished

--- app/src/main/java/com/example/cleanagent/LandingPage.java
+++ app/src/main/java/com/example/cleanagent/LandingPage.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Verification extends AppCompatActivity {
+public class LandingPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_verification);
+        setContentView(R.layout.activity_landing_page);
     }
 }
