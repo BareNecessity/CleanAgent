21baca045fc0e8ec61602f410eb67b68f870e1fd
Bare Necessities <oluwatosinJAjao@outlook.com>
2020-09-19 07:00:47 -0700

    Wallet and Request pages

---
+++ app/src/main/java/com/example/cleanagent/Grateful.java
@@ -0,0 +1,14 @@
+package com.example.cleanagent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Grateful extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grateful);
+    }
+}
