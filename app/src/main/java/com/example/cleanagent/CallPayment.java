2881c893c02ed41885645aad70b35d3ec233861c
Bare Necessities <oluwatosinJAjao@outlook.com>
2020-09-26 10:28:03 -0700

    Extra Project

---
+++ app/src/main/java/com/example/cleanagent/CallPayment.java
@@ -0,0 +1,14 @@
+package com.example.cleanagent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CallPayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_payment);
+    }
+}
