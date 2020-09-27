ce5009ee1f16e3de361172cced39c4156f8f5216
Bare Necessities <oluwatosinJAjao@outlook.com>
2020-09-27 00:10:44 -0700

    Doctor

---
+++ app/src/main/java/com/example/cleanagent/FinancialHelp.java
@@ -0,0 +1,14 @@
+package com.example.cleanagent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinancialHelp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_financial_help);
+    }
+}
