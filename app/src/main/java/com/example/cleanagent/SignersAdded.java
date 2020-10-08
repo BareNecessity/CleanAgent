725817105c03e706ba34462d46a1d83d12048893
Bare Necessities <oluwatosinJAjao@outlook.com>
2020-10-08 16:46:08 -0700

    Final

---
+++ app/src/main/java/com/example/cleanagent/SignersAdded.java
@@ -0,0 +1,14 @@
+package com.example.cleanagent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignersAdded extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signers_added);
+    }
+}
